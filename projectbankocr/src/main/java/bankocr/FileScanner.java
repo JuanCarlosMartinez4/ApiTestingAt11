57b26dde4f54f9328d9a8a2b5f8ee89a6e04eef0
JuanCarlosMartinez4 <Juan.Martinez@fundacion-jala.org>
2020-01-26 16:29:14 -0400

    bank ocr improved

--- projectbankocr/src/main/java/bankocr/FileScanner.java
+++ projectbankocr/src/main/java/bankocr/FileScanner.java
@@ -10,23 +10,29 @@ public class FileScanner {
     public String scanFile(String filePath) {
         File file  = new File(filePath);
         String resultNumber = new String();
+
         try (Scanner scanner = new Scanner(file, StandardCharsets.UTF_8.name())) {
             resultNumber = readFileContent(scanner);
         } catch (FileNotFoundException e) {
-            e.printStackTrace();
+            resultNumber = e.getMessage();
+        } finally {
+            return resultNumber;
         }
-        return resultNumber;
+
     }
 
     private String readFileContent(Scanner scanner) {
         ArrayList<String> content = new ArrayList<>();
         String number = "";
+        final String DEFAULT_VALUE = "?";
+        final String ERROR = " ILL";
+
         while (scanner.hasNextLine()) {
             content.add(scanner.nextLine());
         }
         if (verifySize(content)) {
             number = splitNumber(content);
-            number = number.contains("?") ? number.concat(" ILL") : number;
+            number = number.contains(DEFAULT_VALUE) ? number.concat(ERROR) : number;
         }
         return number;
     }
@@ -34,6 +40,7 @@ public class FileScanner {
     private boolean verifySize(ArrayList<String> content) {
         boolean isValid = true;
         final int SIZE = 27;
+
         for (String item : content) {
             if (item.length() != SIZE) {
                 isValid = false;
@@ -46,13 +53,18 @@ public class FileScanner {
     private String splitNumber(ArrayList<String> content) {
         StringBuilder number = new StringBuilder();
         String temp = "";
+
         final int FIRST_OBJECT = 0;
         final int SECOND_OBJECT = 1;
         final int THIRD_OBJECT = 2;
+        final int INCREMENT = 3;
+
         int init = 0;
         int last = 3;
+
         NumberStorage numberStorage = new NumberStorage();
         int size = content.get(FIRST_OBJECT).length();
+
         while (last <= size) {
             temp = new String();
             temp += content.get(FIRST_OBJECT).subSequence(init, last);
@@ -60,7 +72,7 @@ public class FileScanner {
             temp += content.get(THIRD_OBJECT).subSequence(init, last);
             number.append(numberStorage.verifyNumber(temp));
             init = last;
-            last = last + 3;
+            last += + INCREMENT;
         }
         return number.toString();
     }
